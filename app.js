9ac2864d734eeddaa4d7ea4df73fd3edf807ea19
fechan <edu.fechan@gmail.com>
2020-08-17 21:45:13 -0700

    use config file and add getDB

--- app.js
+++ app.js
@@ -6,7 +6,22 @@
 const sqlite3 = require("sqlite3");
 const sqlite = require("sqlite");
 const express = require("express");
+const config = require("./config.json");
+
 const app = express();
 
-const PORT = 8000;
-app.listen(process.env.PORT || PORT);
+app.get("/api/publication/:id", async function(req, res){
+  res.type("json");
+  try {
+    let db = await getDB();
+  }
+});
+
+async function getDB() {
+  return await sqlite.open({
+    filename: config["db_filename"],
+    driver: sqlite3.Database
+  });
+}
+
+app.listen(process.env.PORT || config["port"]);
