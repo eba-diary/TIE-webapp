1de9cf9023870a34cf21f3f881c21e21cb1c5b27
fechan <edu.fechan@gmail.com>
2020-08-25 18:53:08 -0700

    alphabetically sort returned travelers

--- app.js
+++ app.js
@@ -155,6 +155,13 @@ app.get("/api/travelers/", async function(req, res, next){
       }
     }
     travelers = Object.values(travelers);
+    travelers.sort(function(a, b) {
+      let nameA = a.name.toUpperCase();
+      let nameB = b.name.toUpperCase();
+      if (nameA < nameB) return -1
+      if (nameA > nameB) return 1
+      return 0;
+    });
     res.send(travelers);
   } catch (error) {
     next(error);
