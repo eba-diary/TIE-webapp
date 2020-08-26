4e7bd5dca7cd7328d61e091bffc6470d522f7879
fechan <edu.fechan@gmail.com>
2020-08-25 18:40:21 -0700

    write travelers-list logic

--- views/js/travelers-list.mjs
+++ views/js/travelers-list.mjs
@@ -3,3 +3,42 @@
  */
 "use strict";
 window.addEventListener("load", init);
+
+/**
+ * Initializes the page, fetching the traveler list
+ */
+function init() {
+  fetch("/api/travelers")
+    .then(checkStatus)
+    .then(res => res.json())
+    .then(json => showTravelers(json))
+}
+
+/**
+ * Adds the travelers to the list on the page
+ */
+function showTravelers(travelers) {
+  let travelerList = document.getElementById("travelers");
+  for (let traveler of travelers) {
+    let entry = document.getElementById("entry").content.cloneNode(true);
+    entry.querySelector(".author").textContent = traveler["name"];
+    entry.querySelector(".nationality").textContent = traveler["nationality"];
+    let pubList = entry.querySelector(".publications");
+    for (let publication of traveler["publications"]) {
+      let pubEntry = document.createElement("li");
+      pubEntry.textContent = publication["title"];
+      pubList.append(pubEntry);
+    }
+    travelerList.appendChild(entry);
+  }
+}
+
+/**
+ * Checks if a fetch response is OK, throws otherwise
+ * @param {Response} response fetch response
+ * @returns {Response} fetch response
+ */
+function checkStatus(response) {
+  if (response.ok) return response;
+  throw Error("Error in request: " + response.statusText)
+}
