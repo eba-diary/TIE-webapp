8d5323c214045d5310f5d55b9501a331d7eb1db3
fechan <edu.fechan@gmail.com>
2020-08-27 22:22:55 -0700

    insert mirador viewer

--- views/js/publication.mjs
+++ views/js/publication.mjs
@@ -12,4 +12,40 @@ window.addEventListener("load", init);
  */
 function init() {
   let id = new URLSearchParams(window.location.search).get("id");
+  fetch("/api/publications/" + id)
+    .then(checkStatus)
+    .then(res => res.json())
+    .then(json => showPublication(json));
+}
+
+function showPublication(publication) {
+  document.getElementById("title").innerHTML = publication["title"];
+  let iiifURL = publication["iiif"];
+  Mirador.viewer({
+    id: "mirador-viewer",
+    manifests: {
+      iiifURL: {
+        provider: "Internet Archive"
+      }
+    },
+    windows: [
+      {
+        loadedManifest: iiifURL,
+        canvasIndex: 2,
+      }
+    ],
+    window: {
+      allowClose: false,
+      defaultSideBarPanel: 'info',
+      defaultView: 'gallery',
+      sideBarOpenByDefault: false,
+      hideWindowTitle: true
+    },
+    thumbnailNavigation: {
+      defaultPosition: 'off',
+    },
+    workspaceControlPanel: {
+      enabled: false,
+    },
+  });
 }
