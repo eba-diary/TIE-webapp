65d9bc7da26e370adbfa6ed7f11f1ed5e6bc5f4e
fechan <edu.fechan@gmail.com>
2020-09-01 23:41:50 -0700

    serve 404 if not found

--- routers/client-router.js
+++ routers/client-router.js
@@ -2,26 +2,26 @@
  * @fileoverview client-router.js - Express router for front-end, client pages
  */
 
- const express = require("express");
- const router = express.Router();
+const express = require("express");
+const router = express.Router();
+
+// Names of pages to expose to the front-end by directly going to them
+const PAGES = [
+  "home",
+  "publication",
+  "publications-list",
+  "travelers-list",
+  "search"
+]
 
 router.use("/js", express.static("views/js"));
 router.use("/css", express.static("views/css"));
 
-router.get("/", function(req, res){
-  res.render("pages/home");
+router.get("/:page", function(req, res){
+  if (PAGES.includes(req.params.page)) {
+    res.render("pages/" + req.params.page);
+  } else {
+    res.status(404).render("pages/404");
+  }
 });
-
-router.get("/publications-list", function(req, res){
-  res.render("pages/publications-list");
-});
-
-router.get("/travelers-list", function(req, res){
-  res.render("pages/travelers-list");
-});
-
-router.get("/publication", function(req, res){
-  res.render("pages/publication");
-});
-
 module.exports = router;
